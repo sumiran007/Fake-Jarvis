fb09ad918a70e66425e22722f83756f4c8e86f5c
sumiran007 <sumiranracherla@gmail.com>
2024-12-09 20:09:28 +0000

    changes

--- filechanger.py
+++ filechanger.py
@@ -3,4 +3,4 @@ import sys
 format = input("What should I name the files as?")
 filetpye = input("What is the file type?")
 for i in range(1, len(sys.argv)):
-    os.rename(sys.argv[i], f"{format}{i}.{filetpye}")
+    os.rename(f"{format}{i}.{filetpye}")
